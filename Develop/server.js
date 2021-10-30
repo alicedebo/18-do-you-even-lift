e1ca5b578aa675d4438d450f9a46e7e45cbd67bf
Allison Richardel <airichardel@gmail.com>
2021-10-30 12:55:37 -0600

    file structure setup

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,29 @@
-const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
-const app = express();
-const PORT = process.env.PORT || 3000;
-//const db = require('./app/models');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-app.use(logger("dev"));
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+app.use(express.static('public'));
 
-//require("./app/routes/apiRoutes.js")(app);
-//require("./app/routes/htmlRoutes.js")(app);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require('./routes/views.js'));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
